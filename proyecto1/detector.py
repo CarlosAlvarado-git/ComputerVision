e938315de49e3ad0c5c3bbc64d8f51351e4345ff
CarlosAlvarado-git <61555440+CarlosAlvarado-git@users.noreply.github.com>
2023-10-01 22:13:53 -0600

    avances, pero ya casi
    falta recortar el contorno de la letra y meterlo en el modelo

--- proyecto1/detector.py
+++ proyecto1/detector.py
@@ -19,37 +19,8 @@ def view(img, title=None):
     plt.axis('off')
     plt.show()
 
-
-
-# path = '/Users/carlosalvarado/Desktop/ComputerVision/proyecto1/fotos_placas/'
-# filename = path+'placa9.jpg'
-# im = cv.imread(filename)
-def main():
-    image_path = ''
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument("-p", "--p", help="image path")
-
-    args = argParser.parse_args()
-    print("args=%s" % args)
-    print("args.p=%s" % args.p)
-    image_path = args.p
-    print ('image_path is ', image_path)
-    #path = '/Users/carlosalvarado/Desktop/ComputerVision/proyecto1/fotos_placas/'
-    filename = image_path
-    print(filename)
-    # si uso
-    plt.style.use('dark_background') 
-    im = cv.imread(filename)
-    assert im is not None, "file could not be read, check with os.path.exists()"
-    imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-    print(imgray.shape)
-    h_img = imgray.shape[0]
-    w_img = imgray.shape[1]
-    # # floddfill
-    # im_floodfill = imgray.copy()
-    # D = 65
-    # cv.floodFill(im_floodfill, None, (0 ,0), 255,  loDiff=D, upDiff=D, flags=cv.FLOODFILL_FIXED_RANGE)
-    b = 5
+def contornos_hierarchy(imgray, b):
+    b = b
     #blur 
     img_blur = cv.blur(imgray,(b,b))
     # adaptive Th
@@ -59,7 +30,9 @@ def main():
     mode = cv.RETR_TREE # contour retrieval mode
     method = [cv.CHAIN_APPROX_NONE, cv.CHAIN_APPROX_SIMPLE] # contour approximation method 
     contours, hierarchy = cv.findContours(imgbin, mode, method[1])
+    return contours, hierarchy
 
+def get_placa(contours, h_img, w_img):
     # detecto si tengo placa
     contorno = []
     for c in contours:
@@ -92,21 +65,48 @@ def main():
             else:
                 contorno_placa.pop()
                 contorno_placa.append(c)
+    return contorno_placa
+# path = '/Users/carlosalvarado/Desktop/ComputerVision/proyecto1/fotos_placas/'
+# filename = path+'placa9.jpg'
+# im = cv.imread(filename)
+def main():
+    image_path = ''
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-p", "--p", help="image path")
+
+    args = argParser.parse_args()
+    print("args=%s" % args)
+    print("args.p=%s" % args.p)
+    image_path = args.p
+    print ('image_path is ', image_path)
+    #path = '/Users/carlosalvarado/Desktop/ComputerVision/proyecto1/fotos_placas/'
+    filename = image_path
+    print(filename)
+    # si uso
+    plt.style.use('dark_background') 
+    im = cv.imread(filename)
+    assert im is not None, "file could not be read, check with os.path.exists()"
+    imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+    print(imgray.shape)
+    h_img = imgray.shape[0]
+    w_img = imgray.shape[1]
+    ## obtener contrornos
+    contours, hierarchy = contornos_hierarchy(imgray, 5)
+    ## obtener placa
+    contorno_placa = get_placa(contours, h_img, w_img)
     print(len(contorno_placa))
-
-    # pongo los contornos con color
-    colores = {}
-    indexes = []
-    thickness = 1
-    #(255,0,0),602:(0,255,0)
-    for cnt in range(len(contorno_placa)):
-        #print(f"{w}, {h}")
-        colores.update({cnt:(0,255,0)})
-        indexes.append(cnt)
-
-
+    
     # valido si hay placa
     if (len(contorno_placa) != 0): # si tengo placa, extraigo
+        # pongo los contornos con color
+        colores = {}
+        indexes = []
+        thickness = 1
+        #(255,0,0),602:(0,255,0)
+        for cnt in range(len(contorno_placa)):
+            #print(f"{w}, {h}")
+            colores.update({cnt:(0,255,0)})
+            indexes.append(cnt)
         placa = im.copy()
         x,y,w,h = cv.boundingRect(contorno_placa[0])
         placa = placa[y:y+h, x:x+w]
@@ -116,8 +116,6 @@ def main():
         h_img = imgray.shape[0]
         w_img = imgray.shape[1]
         centro = [int(h_img/2), int(w_img/2)]
-        print(centro[0])
-        print(centro[1])
         # floddfill
         im_floodfill = imgray.copy()
         D = 65
@@ -134,32 +132,24 @@ def main():
         contours, hierarchy = cv.findContours(imgbin, mode, method[1])
 
         contorno = []
-        areas = []
+        var = -1
+        while (len(contorno) < 3):
+            for c in range(len(contours)):   
+                # limitar el contorno, para tener un área más ajustada. 
+                percent =0.052
+                epsilon = percent*cv.arcLength(contours[c],True)
+                approx = cv.approxPolyDP(contours[c],epsilon,True)
+                if (cv.contourArea(approx)>130): #pongo el mínimo del área
+                    area = cv.contourArea(contours[c])
+                    x,y,w,h = cv.boundingRect(contours[c])
+                    if (hierarchy[0][c][-1] == var and y > 10):
+                        if (w > (h*1.35) or w >= (h*1.2)):
+                            print("PASE POR AQUI")
+                        else:
+                            print(hierarchy[0][c])
+                            contorno.append(contours[c])
+            var = var + 1
 
-        for c in contours:
-            # limitar el contorno, para tener un área más ajustada. 
-            percent =0.052
-            epsilon = percent*cv.arcLength(c,True)
-            approx = cv.approxPolyDP(c,epsilon,True)
-            if cv.contourArea(approx) > 50:
-                areas.append(cv.contourArea(approx))
-
-        moda = st.mode(areas)
-        print(areas)
-        print(moda)
-        for c in contours:   
-            # limitar el contorno, para tener un área más ajustada. 
-            percent =0.052
-            epsilon = percent*cv.arcLength(c,True)
-            approx = cv.approxPolyDP(c,epsilon,True)
-            if (cv.contourArea(approx)>130): #pongo el mínimo del área
-                area = cv.contourArea(c)
-                x,y,w,h = cv.boundingRect(c)
-                if (w > (h*1.35) or w >= (h*1.2)):
-                    pass
-                else:
-                    if (h > (w*1.35) or h >= (w*1.2)):
-                        contorno.append(c)
 
         colores = {}
         indexes = []
@@ -170,6 +160,7 @@ def main():
             colores.update({cnt:(0,255,0)})
             indexes.append(cnt)
 
+        #print(hierarchy)
         r = placa.copy()
         # cv.rectangle(im.copy(),(x,y),(x+w,y+h),(0,255,0),1)
         for c in indexes:
@@ -178,32 +169,19 @@ def main():
         view(r)
     else: # si no detecto placa, de una voy a las letras
         contorno = []
-        areas = []
-
-        for c in contours:
-            # limitar el contorno, para tener un área más ajustada. 
+        for c in range(len(contours)):   
+        # limitar el contorno, para tener un área más ajustada. 
             percent =0.052
-            epsilon = percent*cv.arcLength(c,True)
-            approx = cv.approxPolyDP(c,epsilon,True)
-            if cv.contourArea(approx) > 50:
-                areas.append(cv.contourArea(approx))
-
-        moda = st.mode(areas)
-        print(areas)
-        print(moda)
-        for c in contours:   
-            # limitar el contorno, para tener un área más ajustada. 
-            percent =0.052
-            epsilon = percent*cv.arcLength(c,True)
-            approx = cv.approxPolyDP(c,epsilon,True)
+            epsilon = percent*cv.arcLength(contours[c],True)
+            approx = cv.approxPolyDP(contours[c],epsilon,True)
             if (cv.contourArea(approx)>130): #pongo el mínimo del área
-                area = cv.contourArea(c)
-                x,y,w,h = cv.boundingRect(c)
-                if (w > (h*1.35) or w >= (h*1.2)):
-                    pass
-                else:
-                    if (h > (w*1.35) or h >= (w*1.2)):
-                        contorno.append(c)
+                area = cv.contourArea(contours[c])
+                x,y,w,h = cv.boundingRect(contours[c])
+                if (hierarchy[0][c][-1] == -1 and (y > 0 and y < (int(h_img/2)) )):
+                    if (w > (h*1.35) or w >= (h*1.2)):
+                        print("PASE POR AQUI")
+                    else:
+                        contorno.append(contours[c])
 
         colores = {}
         indexes = []
